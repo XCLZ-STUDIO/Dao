2bf9fb4a4b476d621b7dcc6c3600f6e8f0fe803a
Benign X <1341398182@qq.com>
2025-11-20 02:41:05 +0800

    feat(interoperation): support macro to define dims

--- src/entities.rs
+++ src/entities.rs
@@ -58,70 +58,45 @@ impl<T: Float, const DIM: usize> Magable<T, DIM> for Particle<T, DIM> {
     }
 }
 
-impl<T: Float> Interoperable<T, T, 3> for Particle<T, 3> {
-    fn interact(&mut self, obj: &Particle<T, 3>) {
-        let sel_pos = self.position();
-        let obj_pos = obj.position();
+macro_rules! define_interoperable_for_dim {
+    ($ty:tt, $dim:expr, $($indexes:tt),+) => {
+        impl<T: $ty> Interoperable<T, T, $dim> for Particle<T, $dim> {
+            fn interact(&mut self, obj: &Particle<T, $dim>) {
+                let sel_pos = self.position();
+                let obj_pos = obj.position();
+                let dis_2_3 = ((T::zero()$(
+                    +(*sel_pos.get($indexes) - *obj_pos.get($indexes)).powi(2)
+                )*)).sqrt().powi(3);
 
-        let dis_2_3 = ((*sel_pos.get(0) - *obj_pos.get(0)).powi(2)
-            + (*sel_pos.get(1) - *obj_pos.get(1)).powi(2)
-            + (*sel_pos.get(2) - *obj_pos.get(2)).powi(2)).sqrt().powi(3);
+                let g: T = One::one();
 
-        let g: T = One::one();
+                let gra_acc_dlt_mod = g * obj.mas_intensity(obj.pos) / dis_2_3;
+                let acc = Intensity::new([
+                    $(
+                        gra_acc_dlt_mod * (*obj_pos.get($indexes) - *sel_pos.get($indexes))
+                    ),+
+                ]);
+                self.acc = acc;
+            }
 
-        let gra_acc_dlt_mod = g * obj.mas_intensity(obj.pos) / dis_2_3;
-        let acc = Intensity::new([
-            gra_acc_dlt_mod * (*obj_pos.get(0) - *sel_pos.get(0)),
-            gra_acc_dlt_mod * (*obj_pos.get(1) - *sel_pos.get(1)),
-            gra_acc_dlt_mod * (*obj_pos.get(2) - *sel_pos.get(2))
-        ]);
-        self.acc = acc;
-    }
+            fn update(&mut self, t: T) {
+                self.vel = Intensity::new([
+                    $(
+                        *self.vel.get($indexes) + *self.acc.get($indexes) * t
+                    ),+
+                ]);
 
-    fn update(&mut self, t: T) {
-        self.vel = Intensity::new([
-            *self.vel.get(0) + *self.acc.get(0) * t,
-            *self.vel.get(1) + *self.acc.get(1) * t,
-            *self.vel.get(2) + *self.acc.get(2) * t
-        ]);
-
-        let tmp_i = Point::new([
-            *self.pos.get(0) + *self.vel.get(0) * t,
-            *self.pos.get(1) + *self.vel.get(1) * t,
-            *self.pos.get(2) + *self.vel.get(2) * t
-        ]);
-        self.pos = tmp_i;
-    }
-}
-
-impl<T: Float> Interoperable<T, T, 2> for Particle<T, 2> {
-    fn interact(&mut self, obj: &Particle<T, 2>) {
-        let sel_pos = self.position();
-        let obj_pos = obj.position();
-
-        let dis_2_3 = ((*sel_pos.get(0) - *obj_pos.get(0)).powi(2)
-            + (*sel_pos.get(1) - *obj_pos.get(1)).powi(2)).sqrt().powi(3);
-
-        let g: T = One::one();
-
-        let gra_acc_dlt_mod = g * obj.mas_intensity(obj.pos) / dis_2_3;
-        let acc = Intensity::new([
-            gra_acc_dlt_mod * (*obj_pos.get(0) - *sel_pos.get(0)),
-            gra_acc_dlt_mod * (*obj_pos.get(1) - *sel_pos.get(1)),
-        ]);
-        self.acc = acc;
-    }
-
-    fn update(&mut self, t: T) {
-        self.vel = Intensity::new([
-            *self.vel.get(0) + *self.acc.get(0) * t,
-            *self.vel.get(1) + *self.acc.get(1) * t,
-        ]);
-
-        let tmp_i = Point::new([
-            *self.pos.get(0) + *self.vel.get(0) * t,
-            *self.pos.get(1) + *self.vel.get(1) * t,
-        ]);
-        self.pos = tmp_i;
+                let tmp_i = Point::new([
+                    $(
+                        *self.pos.get($indexes) + *self.vel.get($indexes) * t
+                    ),+
+                ]);
+                self.pos = tmp_i;
+            }
+        }
     }
 }
+
+define_interoperable_for_dim!(Float, 2, 0, 1);
+define_interoperable_for_dim!(Float, 3, 0, 1, 2);
+define_interoperable_for_dim!(Float, 4, 0, 1, 2, 3);
